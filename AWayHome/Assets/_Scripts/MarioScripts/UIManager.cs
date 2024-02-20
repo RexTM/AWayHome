66631c0ef07ff2d1676b28ac7e90f8f0b1c97a91
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2024-02-19 21:11:10 -0800

    Currency System (Visible on play)

---
+++ AWayHome/Assets/_Scripts/MarioScripts/UIManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : PlayerData
+{
+    UIManager uiManager;
+    public TMP_Text WishBones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WishBones.text = "Wish Bones: " + wishBones;
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(150, 200, 100, 50), "Add WishBone"))
+        {
+            wishBones += 2;
+        }
+
+        if(GUI.Button(new Rect(10, 200, 100, 50), "Buy Choice"))
+        {
+            if(wishBones >= 5)
+            {
+                wishBones -= 5;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
