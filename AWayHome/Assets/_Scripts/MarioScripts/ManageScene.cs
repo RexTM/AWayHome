ee1a8d7ee084bd7fe8f019d4e4d21b925d62a73d
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2024-03-17 17:22:45 -0700

    Bubble Game Back To Story

--- AWayHome/Assets/_Scripts/MarioScripts/ManageScene.cs
+++ AWayHome/Assets/_Scripts/MarioScripts/ManageScene.cs
@@ -5,64 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class ManageScene : MonoBehaviour
 {
-    public Scene currentScene;
-    public Scene miniGameScene;
+    public static int gameSceneIndex;
+    public static int tempSceneIndex;
 
-    [SerializeField]
-    static public int _sceneCount = 0;
-    static public bool _changeScene = false;
+    [Header("Use this to force a scene after the bubble minigame")]
+    public bool forceSceneSelection = false;
+    [Header("(Use Index from build order)")]
+    public int forcedScene = 0;
 
-    private void Update()
+    private void Start()
     {
-        ChangeScene();
+        gameSceneIndex = PlayerPrefs.GetInt("LastGameplayScene", 0);
+        Debug.Log(gameSceneIndex);
     }
 
-    public void ChangeScene()
+    public void bubleGameTransition()
     {
-        if (_changeScene)
+        gameSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        tempSceneIndex = gameSceneIndex;
+        SceneManager.LoadScene(15);
+    }
+
+    public void returnToStory()
+    { 
+        if (forceSceneSelection)
         {
-            switch (_sceneCount)
-            {
-                case 0:
-                    Debug.Log("MainMenu");
-                    break;
-                case 1:
-                    Debug.Log("HowToPlay");
-                    break;
-                case 2:
-                    Debug.Log("Minigame");
-                    break;
-                case 3:
-                    Debug.Log("Story1");
-                    break;
-                case 4:
-                    Debug.Log("Story2");
-                    break;
-                case 5:
-                    Debug.Log("Story3");
-                    break;
-                case 6:
-                    Debug.Log("Story4");
-                    break;
-                case 7:
-                    Debug.Log("Story5");
-                    break;
-                case 8:
-                    Debug.Log("Story6");
-                    break;
-                case 9:
-                    Debug.Log("Story7");
-                    break;
-                case 10:
-                    Debug.Log("Story8");
-                    break;
-                case 11:
-                    Debug.Log("Story9");
-                    break;
-                default:
-                    break;
-            }
-            _changeScene = !_changeScene;
+            SceneManager.LoadScene(forcedScene);
+        }
+        else
+        {
+            tempSceneIndex++;
+            SceneManager.LoadScene(tempSceneIndex);
         }
     }
 
